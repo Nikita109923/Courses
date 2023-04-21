da25f2f3edef7602b5da60f7e302cbeb810e9497
Nikita Polezaev <polezaev109923@gmail.com>
2023-04-21 12:19:40 +0300

    Add new changes in package SpringApp

---
+++ SpringApp/src/main/java/by/alishev/lessons/reading_from_file/ReadingFromFile.java
@@ -0,0 +1,27 @@
+package by.alishev.lessons.reading_from_file;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ReadingFromFile {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        String path = "aaa.txt";
+        File file = new File(path);
+
+        Scanner scanner = new Scanner(file);
+        String line = scanner.nextLine();
+        String numbersString[] = line.split(" ");
+        int[] numbers = new int[3];
+        int counter = 0;
+
+        for (String number : numbersString) {
+         numbers [counter++]  =   Integer.parseInt(number);
+        }
+        System.out.println(Arrays.toString(numbers));
+
+        scanner.close();
+    }
+}
