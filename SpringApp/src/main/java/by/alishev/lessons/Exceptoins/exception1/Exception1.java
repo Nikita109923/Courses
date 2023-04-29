7d5ad98f60003ef4da7f53ba3c4da7f267eaaac3
Nikita Polezaev <polezaev109923@gmail.com>
2023-04-30 00:03:03 +0300

    Add new changes in package SpringApp

---
+++ SpringApp/src/main/java/by/alishev/lessons/Exceptoins/exception1/Exception1.java
@@ -0,0 +1,19 @@
+package by.alishev.lessons.Exceptoins.exception1;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Exception1 {
+    public static void main(String[] args) {
+        try {
+            readFile();
+        } catch (FileNotFoundException e) {
+            System.out.println("Catch exception in main method");
+        }
+    }
+    public static void  readFile() throws FileNotFoundException {
+    File file = new File("aa.txt");
+    Scanner scan = new Scanner(file);
+}
+}
---
+++ SpringApp/src/main/java/by/alishev/lessons/Exceptoins/exception2/ScannerException.java
@@ -0,0 +1,7 @@
+package by.alishev.lessons.Exceptoins.exception2;
+
+public class ScannerException extends Exception {
+    public ScannerException(String massage){
+        super(massage);
+    }
+}
