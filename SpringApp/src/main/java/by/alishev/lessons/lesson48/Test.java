74b04d57db68e621fc809898667fb2b485266872
Nikita Polezaev <polezaev109923@gmail.com>
2023-05-02 21:00:16 +0300

    Add new changes in SpringApp package, Enums feature

--- SpringApp/src/main/java/by/alishev/lessons/lesson48/Test.java
+++ SpringApp/src/main/java/by/alishev/lessons/lesson48/Test.java
@@ -2,28 +2,15 @@ package by.alishev.lessons.lesson48;
 
 public class Test {
     public static void main(String[] args) {
-        EAnimal animal = EAnimal.CAT;
-        System.out.println(animal);
-
-        //
-//        switch (animal){
-//            case CAT:
-//                System.out.println("it is a cat");
-//                break;
-//            case FROG:
-//                System.out.println("it is a frog");
-//                break;
-//        }
-
         ESeason season = ESeason.SUMMER;
-        System.out.println(season.getTemperature());
+        EAnimal animal = EAnimal.CAT;
+        System.out.println(animal.name());
 
-//        if (season == ESeason.SUMMER){
-//            System.out.println("it's warm outside");
-//        } else if (season== ESeason.SPRING) {
-//            System.out.println("it's okay outside");
-//        }else {
-//            System.out.println("it's colt");
-//        }
+        EAnimal cat = EAnimal.valueOf("CAT");
+        System.out.println(cat.getTranslation());
+
+        ESeason season1 = ESeason.WINTER;
+        System.out.println(season1.ordinal());
+
     }
 }
